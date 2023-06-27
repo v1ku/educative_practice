14d8cb6b2ad22584496924222499c29de476ed28
Vishnu Ajay <vishnu.a.nair1995@gmail.com>
2023-06-27 14:17:01 +0530

    templates

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 def index(request):
-    return HttpResponse("Hello World!")
+
+    my_dict = {'insert_me': "Hello I am from views.py!"}
+    return render(request, 'first_app/index.html', context=my_dict)
 
 def home(request):
     return HttpResponse("Welcome to home page!")
